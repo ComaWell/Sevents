75f3e34d3f07d444ac130ebf6f11cbbf667c93e9
Connor Wellington <connor.matthew.wellington@gmail.com>
2021-11-15 23:50:05 -0500

    Update/Add new cancellable listener utils

--- src/me/connor/event/Listeners.java
+++ src/me/connor/event/Listeners.java
@@ -6,21 +6,38 @@ import me.connor.util.*;
 
 public class Listeners {
 
-	public static Consumer<Cancellable> handleCancelled(@Nonnull Runnable ifCancelled) {
+	public static <T> Consumer<Cancellable<T>> handleCancelled(@Nonnull Consumer<T> ifCancelled) {
 		Assert.notNull(ifCancelled);
 		return (c) -> {
-			if (c.isCancelled()) ifCancelled.run();
+			if (c.isCancelled()) ifCancelled.accept(c.value());
 		};
 	}
 	
-	public static Consumer<Cancellable> handleNotCancelled(@Nonnull Runnable ifNotCancelled) {
+	public static <T> Consumer<Cancellable<T>> handleCancelled(@Nonnull Runnable ifCancelled) {
+		Assert.notNull(ifCancelled);
+		return handleCancelled(() -> ifCancelled.run());
+	}
+	
+	public static <T> Consumer<Cancellable<T>> handleNotCancelled(@Nonnull Consumer<T> ifNotCancelled) {
 		Assert.notNull(ifNotCancelled);
 		return (c) -> {
-			if (!c.isCancelled()) ifNotCancelled.run();
+			if (!c.isCancelled()) ifNotCancelled.accept(c.value());
 		};
 	}
 	
-	public static Consumer<Cancellable> handleCancellable(@Nonnull Runnable ifCancelled, @Nonnull Runnable ifNotCancelled) {
+	public static <T> Consumer<Cancellable<T>> handleNotCancelled(@Nonnull Runnable ifNotCancelled) {
+		Assert.notNull(ifNotCancelled);
+		return handleNotCancelled(() -> ifNotCancelled.run());
+	}
+	
+	public static <T> Consumer<Cancellable<T>> handleCancellable(@Nonnull Consumer<T> ifCancelled, @Nonnull Consumer<T> ifNotCancelled) {
+		Assert.allNotNull(ifCancelled, ifNotCancelled);
+		return (c) -> {
+			(c.isCancelled() ? ifCancelled : ifNotCancelled).accept(c.value());
+		};
+	}
+	
+	public static <T> Consumer<Cancellable<T>> handleCancellable(@Nonnull Runnable ifCancelled, @Nonnull Runnable ifNotCancelled) {
 		Assert.allNotNull(ifCancelled, ifNotCancelled);
 		return (c) -> {
 			(c.isCancelled() ? ifCancelled : ifNotCancelled).run();
